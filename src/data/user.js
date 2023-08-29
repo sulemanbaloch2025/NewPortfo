1ef8705bfcd188543f65bf635a788d4476609bd6
Your Name <you@example.com>
2023-08-29 00:40:48 -0400

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,100 @@
+const INFO = {
+	main: {
+		title: "Suleman's Portfolio",
+		name: "Suleman Baloch",
+		email: "sulemanbashirbaloch2024@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/sulemanbaloch2025",
+		linkedin: "https://www.linkedin.com/in/suleman-baloch-36885726b/",
+		instagram: "https://www.instagram.com/sullyyy2002/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Hi, I am Suleman Baloch: a Software Engineer.",
+		description:
+		"Welcome to my Portfolio! I am a professional developer with a high level of expertise in both front-end and back-end frameworks. Currently pursuing a Bachelor's degree in Computer Science from DePauw University, I have successfully completed two software engineering internships. I plan to graduate by May 2024 and am actively interviewing for software engineering roles.",
+	},
+
+	about: {
+		title: "A little about me:",
+		description:
+			"I am a senior at DePauw University, pursuing a Bachelor's degree in Computer Science. Currently, I hold the position of president in the International Computer Science Association. My passion for technology started in middle school, and I have since actively participated in computer science societies and competitions. I possess extensive experience in various front-end and back-end technologies, including React.js, Next.js, Node.js, AWS, MongoDB, and Restful APIs. Moreover, I have proficiency in several programming languages, such as Java, JavaScript, Python, C++, C#, and CSS. Recently, I focused on learning MERN stack tools, and I am now determined to enhance my knowledge in machine learning. ",
+	},
+
+	articles: {
+		title: "Software Engineering: My Journey",
+		description:
+			"I have been actively learning tools to become a full-stack software engineer. The following highlights my experiences:",
+	},
+
+	projects: [
+		{
+			title: "Web portfolio for the International Computer Science Association",
+			description:
+				" Developed a centralized website using Next.js, TypeScript, and Tailwind CSS to showcase society projects and research for 500+ students. Utilized Amazon S3 for cloud-based multimedia content storage. Technologies: Next.js, TypeScript, Tailwind CSS, AWS.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/typescript/typescript_256x256.png",
+			linkText: "View Project",
+			link: "https://icsa-portfolio-emvr-sulemanbaloch2025.vercel.app",
+		},
+
+		{
+			title: "Inventory Management System for Computer Science Department",
+			description:
+				" Created inventory system with Node.js, Express.js, GraphQL, React.js, MongoDB Atlas, and Bootstrap, earning 95% lab assistant satisfaction. Improved UI with React.js and Bootstrap, and reduced data latency by 30% through GraphQL and Apollo API design.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/sulemanbaloch2025/Inventory-Management-System",
+		},
+
+		{
+			title: "Society Workshop Registration Portal",
+			description:
+				"Created a user-friendly registration portal with React.js and Node.js for over 100 students. Incorporated data handling via Axios and ensured secure token storage and retrieval using JavaScript's local storage API",
+			logo: "https://brandeps.com/logo-download/N/Node-JS-logo-vector-01.svg",
+			linkText: "View Project",
+			link: "https://github.com/sulemanbaloch2025/Society-Workshop-Registration-Portal-",
+		},
+		{
+			title: "Organic Chemistry MERN Application",
+			description:
+				"Developed a MERN web app, elevating Chemistry students' scores by 45% through organic structure drawing. By optimizing page load times with React.Lazy and React Router, load times improved by 40%. My Node.js/Express.js API efficiently handled 10,000+ HTTP requests/sec, and I maintained a 99.9% MongoDB uptime for chemical structure storage",
+			logo: "https://brandeps.com/logo-download/R/React-logo-vector-01.svg",
+			linkText: "View Project",
+			link: "https://github.com/sulemanbaloch2025/ChemWeb",
+		},
+		{
+			title: "Krypt - Web 3.0 Blockchain Application",
+			description:
+				"Created a Web 3.0 blockchain app with React, Next.js, and Node.js, integrating it with MetaMask for secure Ethereum wallet transactions. Additionally, I designed and deployed Ethereum smart contracts using Solidity.",
+			logo: "https://brandeps.com/logo-download/M/Metamask-logo-vector-01.svg",
+			linkText: "View Project",
+			link: "https://github.com/sulemanbaloch2025/Web-3.0-App",
+		},
+		{
+			title: "eCommerce Application",
+			description:
+				"Developed a Next.js full-stack app for a seamless user experience, boosting conversion rates. I also incorporated Stripe for secure payment processing and integrated a headless CMS like Sanity for streamlined product information management and updates",
+			logo: "https://seeklogo.com/images/N/next-js-logo-7929BCD36F-seeklogo.com.png",
+			linkText: "View Project",
+			link: "https://github.com/sulemanbaloch2025/Shahbaaz-Associates-eCommerce-App",
+		},
+		{
+			title: "Chat Application ",
+			description:
+				" developed a full-stack chat application using socket.io",
+			logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Socket-io.svg/2048px-Socket-io.svg.png",
+			linkText: "View Project",
+			link: "https://github.com/sulemanbaloch2025/RemHives-Chat-App",
+		},
+
+
+	
+	],
+};
+
+export default INFO;
